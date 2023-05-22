1de1136c06cf109a8635ac23aa059a3ee4ca5f8c
Kennedy Chidi <kennedychididev@gmail.com>
2023-05-22 02:17:50 +0100

    good

---
+++ dist/script/smartSupp.js
@@ -0,0 +1,18 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "9be5a24de10e6b9df1d5d1d2887e9407e6ac57e9";
+window.smartsupp ||
+  (function (d) {
+    var s,
+      c,
+      o = (smartsupp = function () {
+        o._.push(arguments);
+      });
+    o._ = [];
+    s = d.getElementsByTagName("script")[0];
+    c = d.createElement("script");
+    c.type = "text/javascript";
+    c.charset = "utf-8";
+    c.async = true;
+    c.src = "https://www.smartsuppchat.com/loader.js?";
+    s.parentNode.insertBefore(c, s);
+  })(document);
